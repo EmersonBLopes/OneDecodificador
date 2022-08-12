5a39485cb0884cb743929f570f1b8aa7ecb43cee
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-08-12 12:35:15 -0300

    Algoritmos de criptografar e desencriptografar prontos

--- js/script.ts
+++ js/script.ts
@@ -1,40 +1,102 @@
-function codificar(texto:string):string{
+function codificar(texto: string): string {
 
-    let novoTexto:string = "";
+    let novoTexto: string = "";
 
-    console.log(texto);
-        for(let i:number=0; i<texto.length;i++){
-            switch (texto[i]){
-                case 'e':
-                    novoTexto = novoTexto + "enter";
-                    break;
-                case 'i':
-                    novoTexto = novoTexto + "imes";
-                    break;
-                case 'a':
-                    novoTexto = novoTexto + "ai";
-                    break;
-                case 'o':
-                    novoTexto = novoTexto + "ober";
-                    break;
-                case 'u':
-                    novoTexto = novoTexto + "ufat";
-                    break;
-                default:
-                    novoTexto = novoTexto + texto[i];
-                    break;
-            }
+    for (let i: number = 0; i < texto.length; i++) {
+        switch (texto[i]) {
+            case 'e':
+                novoTexto = novoTexto + "enter";
+                break;
+            case 'i':
+                novoTexto = novoTexto + "imes";
+                break;
+            case 'a':
+                novoTexto = novoTexto + "ai";
+                break;
+            case 'o':
+                novoTexto = novoTexto + "ober";
+                break;
+            case 'u':
+                novoTexto = novoTexto + "ufat";
+                break;
+            default:
+                novoTexto = novoTexto + texto[i];
+                break;
         }
+    }
     return novoTexto;
 }
 
-let mensagem:string = prompt("Digite um texto:");
+function comparaString(inicioString: number, fimString: number, cmpString: string, texto: string): boolean {
 
-let mensagemCodificada:string = codificar(mensagem.toLowerCase());
-alert(mensagemCodificada);
+    let resposta: boolean = false;
+    if (texto.substring(inicioString, fimString) == cmpString) {
+        resposta = true;
+    }
 
-/*A letra "e" é convertida para "enter"
-A letra "i" é convertida para "imes"
-A letra "a" é convertida para "ai"
-A letra "o" é convertida para "ober"
-A letra "u" é convertida para "ufat"*/
+    return resposta;
+}
+
+function decodificar(texto: string): string {
+
+    let novoTexto: string = "";
+
+    for (let i: number = 0; i < texto.length; i) {
+        switch (texto[i]) {
+            case 'e':
+                if (comparaString(i, i + 5, "enter", mensagem)) {
+                    novoTexto = novoTexto + 'e';
+                    i = i + 5
+                } else {
+                    novoTexto = novoTexto + texto[i];
+                    i++
+                }
+                break
+            case 'i':
+                if (comparaString(i, i + 4, "imes", mensagem)) {
+                    novoTexto = novoTexto + 'i';
+                    i = i + 4;
+                } else {
+                    novoTexto = novoTexto + texto[i];
+                    i++;
+                }
+                break
+            case 'a':
+                if (comparaString(i, i + 2, "ai", mensagem)) {
+                    novoTexto = novoTexto + 'a';
+                    i = i + 2;
+                } else {
+                    novoTexto = novoTexto + texto[i];
+                    i++;
+                }
+                break
+            case 'o':
+                if (comparaString(i, i + 4, "ober", mensagem)) {
+                    novoTexto = novoTexto + 'o';
+                    i = i + 4;
+                } else {
+                    novoTexto = novoTexto + texto[i];
+                    i++;
+                }
+                break
+            case 'u':
+                if (comparaString(i, i + 4, "ufat", mensagem)) {
+                    novoTexto = novoTexto + 'u';
+                    i = i + 4;
+                } else {
+                    novoTexto = novoTexto + texto[i];
+                    i++;
+                }
+                break
+            default:
+                novoTexto = novoTexto + texto[i];
+                i++;
+                break;
+        }
+    }
+    return novoTexto;
+}
+
+let mensagem: string = prompt("Digite um texto:");
+
+console.log(decodificar(mensagem));
